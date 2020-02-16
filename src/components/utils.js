6687fafda50c21c2965350e1985dfb3f5a5c0b6a
sema0710 <pandati@naver.com>
2020-02-16 19:25:02 +0900

    ㅅㅅㅅㅅㅅ

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = "https://dsm-scarfs.hs.kr/chuckflap/";
+const url = "http://dsm-scarfs.hs.kr/chuckflap/";
 const ApiDefault = axios.create({ baseURL : url });
 
 export default ApiDefault;
