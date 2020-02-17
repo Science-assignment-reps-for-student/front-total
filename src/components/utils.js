d73267728a22139a301b1aac962092c9a707b620
sema0710 <pandati@naver.com>
2020-02-17 15:47:17 +0900

    url
    url

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = "https://dsm-scars.hs.kr/chuckflap/";
+const url = "https://dsm-scarfs.hs.kr/chuckflap/";
 const ApiDefault = axios.create({ baseURL : url });
 
 export default ApiDefault;
