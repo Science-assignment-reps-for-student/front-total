faeb02c7c99018e07f9f4ed267a81064b769bd23
sema0710 <pandati@naver.com>
2020-02-21 17:17:47 +0900

    admin  update

---
+++ src/components/Admin_Chatting/Chatting.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Header } from '../public/Header';
+import * as S from './style/ChattingStyle';
+
+const Chatting = () => {
+    return (
+        <>
+            <Header/>
+            <S.ChattingMain>
+                
+            </S.ChattingMain>
+        </>
+    )
+}
+
+export default Chatting;
---
+++ src/components/Admin_Chatting/style/ChattingStyle.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const ChattingMain = styled.div`
+    width: 100%;
+    height: 95vh;
+    background-color: #FFFFFF;
+`
+
+export const ChattingSubHeader = styled.div`
+    width: 100%;
+    height: 5vh;
+    background-color: #FFFFFF;
+`
+
+export const ChattingBubble = styled.div`
+    height: 5vh;
+`
+
+export const ChattingTypeInput = styled.input`
+    width: 80%;
+    height: 90%;
+    margin: 10% 0 10% 0;
+    background-color: #F2F2F2;
+`
+
+export const ChattingType = styled.div`
+    width: 100%;
+    height: 5vh;
+`
+
+export const ChattingButton = styled.div`
+    width: 27px;
+    height: 27px;
+`
