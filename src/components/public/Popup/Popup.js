44a7647030fa7c97271b2ff6f69a56341e3a7f13
leewoochan <030219woo@naver.com>
2020-04-21 21:03:10 +0900

    [UPDATE] <popup> css

--- src/components/public/Popup/Popup.js
+++ src/components/public/Popup/Popup.js
@@ -24,12 +24,13 @@ const Popup = ({ popupOn, setPopupOn, content }) => {
     return (
         <PopupWrapper>
             <Header>
+                <span>[공지] 사이트 관리자입니다.</span>
                 <button onClick={closePopup}>x</button>
             </Header>
             <Content dangerouslySetInnerHTML={{ __html: content }}>
             </Content>
             <Footer onClick={footerClick}>
-                하루동안 열지 않음 [닫기]
+                <span>하루동안 열지 않음 [닫기]</span>
             </Footer>
         </PopupWrapper>
     );
@@ -38,21 +39,37 @@ const Popup = ({ popupOn, setPopupOn, content }) => {
 export default Popup;
 
 const PopupWrapper = styled.div`
+    border: 1px solid #c5c5c5;
+    border-radius: 3px;
     position: absolute;
     top: 0;
     left: 20%;
     width: 400px;
     height: 300px;
     background-color: white;
-    padding: 10px;
+    padding: 2px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
 `;
 
 const Header = styled.header`
+    height: 30px;
+    padding: 0 4px;
+    > span {
+        font-size: 14px;
+        padding-top: 3px;
+        box-sizing: border-box;
+    }
+    color: #333;
+    font-weight: bold;
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
+    align-items: center;
+    border: 1px solid #ddd;
+    background: #e9e9e9;
+    color: #333;
+    border-radius: 3px;
 `;
 
 const Content = styled.p`
@@ -60,9 +77,20 @@ const Content = styled.p`
     height: 80%;
     overflow-y: scroll;
     word-break: break-all;
+    padding: 14px 16px;
 `;
 
 const Footer = styled.p`
     cursor: pointer;
-    text-align: right;
+    padding-right: 10px;
+    background-color: #3b568f;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    height: 20px;
+    color: white;
+    > span {
+        font-size: 14px;
+        padding-top: 3px;
+    }
 `;
